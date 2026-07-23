5e395b42e1dad7f379f61e078cdcbfb153f8ee7d
Tarun <tarun.techtrail@gmail.com>
2026-07-24 01:14:10 +0530

    Final fix production deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://quizgen-ai-okns.onrender.com/",
+  baseURL: "https://quizgen-ai-okns.onrender.com/api",
 });
 
 export const generateQuiz = async (quizData) => {
